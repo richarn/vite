b7408208080114bed06e0d63bc3ca45efebbb2c7
richarn <rnezferreira9@gmail.com>
2022-09-30 22:42:58 -0400

    1rt event

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,10 +1,19 @@
 import PropTypes from 'prop-types';
 
 export const CounterApp = ({ value }) => {
+
+    function handleAdd(event, newValue) { 
+        console.log( newValue ); 
+    }
+
     return (
         <>
             <h1>CounterApp</h1>
             <h2>{ value }</h2>
+
+            <button onClick={ (event) => handleAdd(event, 'hola') }>
+                +1
+            </button>
         </>
     )
 }
