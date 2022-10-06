dd208a1292ca6b4f6600bc1ea16330505659d6e9
richarn <rnezferreira9@gmail.com>
2022-10-06 19:22:26 -0300

    demoComponent using test

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -1,7 +1,14 @@
-test('Debe funcionar', () => { 
 
-    if( 0 === 1){
-        throw new Error('No se puede divir');
-    }
+describe( 'Pruebas en <DemoComponent />',() => {
 
- })
+    test('Esta prueba no debe fallar', () => {
+            //1. inicializacion
+        const message1 = "Hola Mundo";
+        
+        //2. estimulo
+        const message2 = message1.trim();
+        
+        //3. obervar el comportamiento ... esperado
+        expect( message1 ).toBe( message2);
+    });
+});
